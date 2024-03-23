35a719aa7563bf6d82fd222387b7ed1e54ad8093
mrAbduljalil <abduljalilmirzayev@gmail.com>
2024-03-23 21:40:32 +0500

    single_page finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-// import { useState } from 'react'
 
 import './App.css'
 import Single from './components/Uzbek/Uzbek_single_hotel/single'
 
-
-// import Uzbek_single_hotel from './components/Uzbek/Uzbek_single_hotel/Uzbek_single_hotel'
 
 
 function App() {
  return (
     <>
       <Single />
-       {/* <Uzbek_single_hotel /> */}
     </>
   )
 }
